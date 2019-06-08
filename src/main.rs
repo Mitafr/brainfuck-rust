8e9089ab5851e84453d4324e95229a456f8bd3df
Mita <martin.pelletier78@hotmail.com>
2019-06-08 11:07:06 +0200

    Optimized interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,119 @@
 use std::io::stdin;
 use std::num::Wrapping;
 use std::fs;
+use std::io;
+use std::io::Write;
 
 fn main() {
     let mut bf: BrainfuckState = BrainfuckState::new();
-    bf.execute("../samples/bench.bf");
+    bf.parse("../samples/bench.bf");
+    bf.execute();
+}
+
+enum Instruction {
+    Next,
+    Prev,
+    Incr,
+    Decr,
+    Input,
+    Output,
+    JumpOpen(usize),
+    JumpClose(usize),
+    Comment
+}
+
+impl From<char> for Instruction {
+    fn from(c: char) -> Instruction {
+        match c {
+            '>' => Instruction::Next,
+            '<' => Instruction::Prev,
+            '+' => Instruction::Incr,
+            '-' => Instruction::Decr,
+            '.' => Instruction::Output,
+            ',' => Instruction::Input,
+            '[' => Instruction::JumpOpen(0),
+            ']' => Instruction::JumpClose(0),
+            _ => Instruction::Comment
+        }
+    }
 }
 
 struct BrainfuckState {
     mem: Vec<Wrapping<u8>>,
     pointer: usize,
+    instructions: Vec<Instruction>,
 }
 
 impl BrainfuckState {
     fn new() -> BrainfuckState {
         BrainfuckState {
             mem: vec![Wrapping(0u8); 30000],
-            pointer: 0
+            pointer: 0,
+            instructions: Vec::new(),
         }
     }
 
-    fn execute(&mut self, filename: &str) {
+    fn parse(&mut self, filename: &str) {
         let code = fs::read_to_string(filename)
             .expect(&format!("Something went wrong reading the file {}", filename));
-        let mut i: usize = 0;
-        while i < code.len() {
-            match code.as_bytes()[i] as char {
-                '>' => self.pointer += 1,
-                '<' => self.pointer -= 1,
-                '+' => self.mem[self.pointer] += Wrapping(1u8),
-                '-' => self.mem[self.pointer] -= Wrapping(1u8),
-                '.' => {
-                    print!("{}", self.mem[self.pointer].0 as char);
+        let mut s_brackets = Vec::new();
+        let mut instr_id = 0;
+        for b in code.bytes() {
+            let mut inst: Instruction = Instruction::from(b as char);
+            match inst {
+                Instruction::Comment => {continue;}
+                Instruction::JumpClose(_) => {
+                    let i_open = s_brackets.pop().unwrap();
+                    self.instructions[i_open] = Instruction::JumpOpen(instr_id - i_open);
+                    inst = Instruction::JumpClose(instr_id - i_open);
                 },
-                ',' => {
-                    let mut s: String = String::new();
-                    print!(":");
-                    stdin().read_line(&mut s).expect("Did not enter a correct string");
-                    let b: u8 = s.bytes().nth(0).expect("erreur");
-                    self.mem[self.pointer] = Wrapping(b);
-                }
-                '[' => {
-                    if self.mem[self.pointer].0 == 0 {
-                        let mut counter = 0;
-                        while i < code.len() {
-                            i += 1;
-                            match code.as_bytes()[i] as char {
-                                '[' => {
-                                    counter += 1;
-                                }
-                                ']' => {
-                                    if counter == 0 {
-                                        break;
-                                    }
-                                    counter -= 1;
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
-                },
-                ']' => {
-                    if self.mem[self.pointer].0 != 0 {
-                        let mut counter = 0;
-                        loop {
-                            i -= 1;
-                            match code.as_bytes()[i] as char {
-                                ']' => {
-                                    counter += 1;
-                                }
-                                '[' => {
-                                    if counter == 0 {
-                                        break;
-                                    }
-                                    counter -= 1;
-                                }
-                                _ => {}
-                            }
-                        }
-                        i -= 1;
-                    }
+                Instruction::JumpOpen(_) => {
+                    s_brackets.push(instr_id);
                 },
                 _ => {}
             }
-            i += 1;
+            self.instructions.push(inst);
+            instr_id += 1;
         }
+    }
+    fn execute(&mut self) {
+        let mut i: usize = 0;
+        while i < self.instructions.len() {
+            i = self.step_instr(i);
+        }
+    }
+
+    fn step_instr(&mut self, current_index: usize) -> usize {
+        let mut index_out = current_index;
+        match self.instructions[current_index] {
+            Instruction::Next => self.pointer += 1,
+            Instruction::Prev => self.pointer -= 1,
+            Instruction::Incr => self.mem[self.pointer] += Wrapping(1u8),
+            Instruction::Decr => self.mem[self.pointer] -= Wrapping(1u8),
+            Instruction::Output => {
+                print!("{}", self.mem[self.pointer].0 as char);
+                io::stdout().flush().unwrap();
+            },
+            Instruction::Input => {
+                let mut s: String = String::new();
+                print!(":");
+                stdin().read_line(&mut s).expect("Did not enter a correct string");
+                let b: u8 = s.bytes().nth(0).expect("erreur");
+                self.mem[self.pointer] = Wrapping(b);
+            },
+            Instruction::JumpOpen(offset) => {
+                if self.mem[self.pointer].0 == 0 {
+                    index_out += offset;
+                }
+            },
+            Instruction::JumpClose(offset) => {
+                if self.mem[self.pointer].0 != 0 {
+                    index_out -= offset;
+                }
+            },
+            _ => {}
+        }
+        index_out += 1;
+        return index_out;
     }
 }
